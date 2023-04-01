b181f4b73d4d91abe323643ff264b67f84659652
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-01 22:02:18 +0600

    useContext hook

---
+++ src/tutorial/8-useContext/setup/1-context-api.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from "react";
+import { data } from "../../../data";
+// more components
+// fix - context api, redux (for more complex cases)
+
+const PersonContext = React.createContext();
+//two components - Provider, Consumer
+
+const ContextAPI = () => {
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <PersonContext.Provider value={{ removePerson, people }}>
+      <h3>Context Api/useContext</h3>
+      <List />
+    </PersonContext.Provider>
+  );
+};
+
+const List = () => {
+  const mainData = useContext(PersonContext); //you don't have to destructure. you can do this way too
+  console.log(mainData);
+  return (
+    <>
+      {mainData.people.map((person) => {
+        return <SinglePerson key={person.id} {...person} />;
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext); //destructuring
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>remove</button>
+    </div>
+  );
+};
+
+export default ContextAPI;
