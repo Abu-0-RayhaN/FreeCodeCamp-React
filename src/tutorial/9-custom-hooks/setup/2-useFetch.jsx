a2978d6ebc3e623b5ceaab2ca9ee658a281ee500
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-01 22:36:05 +0600

    Custome hook

---
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+//you must use 'use' before hook component's name
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+  return { loading, products };
+};
