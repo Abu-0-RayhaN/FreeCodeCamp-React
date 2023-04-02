497df8ef1351512d3096ffd96b910c3431b4d77c
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-02 23:49:05 +0600

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Setup from "./tutorial/10-prop-types/setup/index";
+import Setup from "./tutorial/11-react-router/setup";
 export default function App() {
   return (
     <div className="container ">
+      <h2>advanced react</h2>
       <Setup />
     </div>
   );
