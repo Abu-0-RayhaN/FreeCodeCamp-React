9d2749eb13a1b2b4a90dd6a4cd01a012b86b8c3a
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-03 00:36:48 +0600

    useCallback, memo, useMemo final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import Setup from "./tutorial/11-react-router/setup";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 export default function App() {
   return (
     <div className="container ">
-      <h2>advanced react</h2>
       <Setup />
     </div>
   );
